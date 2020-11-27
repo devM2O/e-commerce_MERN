bf72d6e47528da87b3df25e9c9ba2598acd4e62a
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-11-27 21:22:19 +0630

    rating components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import products from '../products';
-import { Col, Row } from 'react-bootstrap';
+import products from '../products'
+import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) =>(
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={6} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default HomeScreen
