fbe36f7414480d4a9f42e54d29e50911dce21826
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-11-30 23:08:13 +0630

    create Redux state in HOME screen

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 
 //routes
 app.use('/api/products', productRoutes)
+
+//Middlewares
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
