26a61ae364b1fcdf46c3571663d318dc10c222c7
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-12-01 23:53:25 +0630

    user authorization

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -11,20 +13,21 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running....')
 })
 
-//routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
-//Middlewares
 app.use(notFound)
 app.use(errorHandler)
-
 const PORT = process.env.PORT || 5000
-
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
